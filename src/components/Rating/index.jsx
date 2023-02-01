b5e659d0db5a4af7e4e7daa7eaf027572f5dc785
Vincent Pastor <vincent.pastor.pro@gmail.com>
2023-02-01 17:03:17 +0100

    Fixe police bug

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,13 +1,17 @@
+import star_red from "../../assets/vectors/star_red.png"
+import star_grey from "../../assets/vectors/star_grey.png"
+
+
 function Rating ({rate}) {
     const rateMax = 5 
     const numberOfColoredStars = rate 
     const numberOfGreyStars = rateMax - numberOfColoredStars
     let starsArray = []
     for (let i=0; i<numberOfColoredStars; i++) {
-        starsArray.push(<div className="rating__coloredStar"><img src='../../assets/vectors/star_red.png' alt="star" /></div>)
+        starsArray.push(<div className="rating__coloredStar" key={i}><img src={star_red} alt="star_red" /></div>)
     }
     for (let i=0; i<numberOfGreyStars; i++) {
-        starsArray.push(<div className="rating__greyStar" key={rateMax-i}><img src="https://img.icons8.com/ios-glyphs/30/null/filled-star.png" alt="star"/></div>)
+        starsArray.push(<div className="rating__greyStar" key={numberOfColoredStars+i+1}><img src={star_grey} alt="star_grey"/></div>)
     }
 
     return (
